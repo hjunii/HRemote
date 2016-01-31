6b0fa71fccd0f16c1f5fc1ba72797049197cda53
hjunii <hjunii@gmail.com>
2016-02-01 01:27:26 +0900

    2016-02-01

--- app/src/main/java/com/android/hremote/DiscoveryFragment.java
+++ app/src/main/java/com/android/hremote/DiscoveryFragment.java
@@ -1,12 +1,14 @@
 package com.android.hremote;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -18,7 +20,6 @@ public class DiscoveryFragment extends Fragment {
 
     private ArrayAdapter<String> mAdapter;
     private NsdHelper mNsdHelper;
-    private ArrayList<NsdServiceInfo> mServiceList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,12 +27,29 @@ public class DiscoveryFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.discovery_fragment, container, false);
 
-        mAdapter = new ArrayAdapter<String>(getContext(),
+        mAdapter = new ArrayAdapter<>(getContext(),
                 android.R.layout.simple_list_item_1);
 
         ListView listView = (ListView) v.findViewById(R.id.listView);
         listView.setAdapter(mAdapter);
         listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Create new fragment and transaction
+                Fragment newFragment = new MousePadFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+                // Replace whatever is in the fragment_container view with this fragment,
+                // and add the transaction to the back stack
+                transaction.replace(R.id.content_fragment, newFragment);
+                transaction.addToBackStack(null);
+
+                // Commit the transaction
+                transaction.commit();
+            }
+        });
 
         mNsdHelper = new NsdHelper(this);
         mNsdHelper.discoveryServices();
@@ -46,7 +64,6 @@ public class DiscoveryFragment extends Fragment {
                     @Override
                     public void run() {
                         mAdapter.add(name);
-                        //mAdapter.notifyDataSetChanged();
                     }
                 });
     }
@@ -58,7 +75,6 @@ public class DiscoveryFragment extends Fragment {
                     @Override
                     public void run() {
                         mAdapter.remove(name);
-                        //mAdapter.notifyDataSetChanged();
                     }
                 });
     }
@@ -70,7 +86,6 @@ public class DiscoveryFragment extends Fragment {
                     @Override
                     public void run() {
                         mAdapter.clear();
-                        //mAdapter.notifyDataSetChanged();
                     }
                 });
     }
---
+++ app/src/main/java/com/android/hremote/MousePadFragment.java
@@ -0,0 +1,27 @@
+package com.android.hremote;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MousePadFragment extends Fragment {
+
+
+    public MousePadFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.mousepad_fragment, container, false);
+
+        return v;
+    }
+
+}
